4c8d446afbfc297e49bd552e1d910caac9c641b6
George Anastasov <georgeanastasov@outlook.com>
2018-08-31 12:48:09 +0300

    worked on global error handling. added test

--- MedHelper.Web/Pages/Error.cshtml.cs
+++ MedHelper.Web/Pages/Error.cshtml.cs
@@ -1,15 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Diagnostics;
 namespace MedHelper.Web.Pages
 {
 	public class ErrorModel : PageModel
 	{
 		public string RequestId { get; set; }
-
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+		public void OnGet(Exception ex) => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
 	}
 }
